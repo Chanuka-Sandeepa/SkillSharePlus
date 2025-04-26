f14a241004c183b174670ba0dcbf274d3c96b6c4
Tharindu <IT22221278@my.sliit.lk>
2025-04-26 17:03:47 +0530

    Create a PostResponse.java
    Create a PostResponse.java for skill sharing

--- skillshareplus/src/main/java/com/example/skillshareplus/controller/PostController.java
+++ skillshareplus/src/main/java/com/example/skillshareplus/controller/PostController.java
@@ -3,7 +3,7 @@ package com.example.skillshareplus.controller;
 import com.example.skillshareplus.dto.request.CommentRequest;
 import com.example.skillshareplus.dto.request.CreatePostRequest;
 import com.example.skillshareplus.dto.response.PagedPostsResponse;
-import com.skillshareplus.dto.response.PostResponse;
+import com.example.skillshareplus.dto.response.PostResponse;
 import com.skillshareplus.security.services.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
