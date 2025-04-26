b65118e29d99d9bc62cc40efafa8b3d3e7d4bdb4
Niroshan100 <tharinduniroshan016@gmail.com>
2025-04-26 18:17:27 +0530

    create LearningPlanResponse and LearningModuleResponse
    create LearningPlanResponse and LearningModuleResponse

---
+++ skillshareplus/src/main/java/com/example/skillshareplus/dto/response/LearningModuleResponse.java
@@ -0,0 +1,19 @@
+package com.example.skillshareplus.dto.response;
+
+import com.SkillShare.SkillShare.model.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LearningModuleResponse {
+    private String id;
+    private String title;
+    private String description;
+    private List<LearningTaskResponse> tasks;
+    private int estimatedHours;
+    private int completedHours;
+}
