0bb6e1578ef5e301abadf0ce9bed1a16bc27b1f1
Tharindu <IT22221278@my.sliit.lk>
2025-04-26 16:38:56 +0530

    Create a CreatePost.java
    Create a CreatePost.java for Skill
     Sharing

--- skillshareplus/src/main/java/com/example/skillshareplus/controller/PostController.java
+++ skillshareplus/src/main/java/com/example/skillshareplus/controller/PostController.java
@@ -1,7 +1,7 @@
 package com.example.skillshareplus.controller;
 
 import com.example.skillshareplus.dto.request.CommentRequest;
-import com.skillshareplus.dto.request.CreatePostRequest;
+import com.example.skillshareplus.dto.request.CreatePostRequest;
 import com.skillshareplus.dto.response.PagedPostsResponse;
 import com.skillshareplus.dto.response.PostResponse;
 import com.skillshareplus.security.services.PostService;
