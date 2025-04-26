dc04c385884490665cdfd378141ed7b0df65bf84
Chanuka <sandeepachanuka88@gmail.com>
2025-04-26 12:52:03 +0530

    Setup the Backend
    Setup the backend part in the project

---
+++ skillshareplus/src/test/java/com/example/skillshareplus/SkillshareplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillshareplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillshareplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
